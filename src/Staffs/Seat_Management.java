7490736a75e41fd7d128a1781a6f1706651d43a5
Azillil <johnlutherdelacruz01@gmail.com>
2024-05-02 00:01:32 +0800

    update lang sa ibang buttons 11:40pm

--- src/Staffs/Seat_Management.java
+++ src/Staffs/Seat_Management.java
@@ -204,12 +204,12 @@ public class Seat_Management extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        this.setVisible(false);
-        mlst.setVisible(true);
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        mlst.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
 
